18bdf163027d88d5f3385e42d0e177da1ae41205
nickapmello <nicolas.mello@edu.unifil.br>
2024-05-04 11:55:45 -0300

    implement

---
+++ Models/Iphone.cs
@@ -0,0 +1,15 @@
+namespace DesafioPOO.Models
+{
+    // Herdando da classe "Smartphone"
+    public class Iphone : Smartphone
+    {
+        public Iphone(string numero, string modelo, string imei, int memoria) 
+            : base(numero, modelo, imei, memoria) { }
+
+        // Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no iPhone");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using DesafioPOO.Models;
+
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nokia nokia = new Nokia("123456789", "ModeloNokia1", "IMEI123456", 64);
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Iphone iphone = new Iphone("987654321", "ModeloIphone1", "IMEI654321", 128);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Telegram");
+        }
+    }
+}
